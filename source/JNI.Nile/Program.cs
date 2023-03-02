148b8b25a81dfddde4127502267159b3538af3f5
Michael Bradvica <mjbradvica@gmail.com>
2023-03-02 13:25:45 -0500

    idk my bff jill

--- source/JNI.Nile/Program.cs
+++ source/JNI.Nile/Program.cs
@@ -1,4 +1,5 @@
-namespace Nile.API
+
+namespace JNI.Nile
 {
     public class Program
     {
@@ -6,12 +7,16 @@ namespace Nile.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Add services to the container.
+
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -21,6 +26,7 @@ namespace Nile.API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
 
             app.MapControllers();
 
